dd0643c56d228e2829dd9e2d4b2d1c70d3bd7c29
Cameron Eldridge <cameldridge@gmail.com>
2025-04-17 19:40:58 -0400

    modernize dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,23 @@
-'use strict';
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+"use strict";
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
-    mode: 'development',
-    entry: './public_html/script/src/index.es6',
-    output: {
-        path: path.resolve('public_html'),
-        filename: 'catan.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader',
-                ],
-            },
-            { test: /\.es6$/ }
-        ]
-    },
-    plugins: [
-        new MiniCssExtractPlugin()
-    ]
+  mode: "development",
+  entry: "./public_html/script/src/index.es6",
+  output: {
+    path: path.resolve("public_html"),
+    filename: "catan.js",
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "fast-sass-loader"],
+      },
+      { test: /\.es6$/ },
+      { test: /\.png$/, type: 'asset/resource' },
+    ],
+  },
+  plugins: [new MiniCssExtractPlugin()],
 };
